e2f99fb037a078553d3fc3daa686c5c7a3d94b26
pcismynme <pcismyname007@gmail.com>
2023-02-13 22:12:51 +0700

    add web project

---
+++ src/DatagramPacket2.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.net.SocketException;
+
+public class DatagramPacket2 extends DatagramSocket{
+
+    public DatagramPacket2() throws SocketException{
+        super();
+    }
+}
